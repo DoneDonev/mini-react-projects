48a8c8631c37de21ca093d994ab9c742b1d719dc
DoneDonev <donedonev15@gmail.com>
2025-04-13 18:11:17 +0200

    added style change to background

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ import Calculator from './components/Calculator'
 
 function App() {
   return(
-    <div className='bg-linear-to-br from-slate-600 to-slate-900 h-dvh w-full flex items-center justify-center relative overflow-clip'>
-         <Calculator />
-    </div>
+    <>
+      <div className='bg-linear-to-br from-slate-600 to-slate-900 h-dvh w-full flex items-center justify-center relative overflow-clip'>
+          <Calculator />
+      </div>
+    </>
   )
 }
 
